a947d8cc53908cdaf4016318e8c06b906a1249d1
ramendra mishra <76106853+Ramendra11n@users.noreply.github.com>
2022-11-05 10:58:20 +0530

    Add files via upload

---
+++ conditonals and loops.py
@@ -0,0 +1,43 @@
+#conditional are the statement that are used to implement the decision making using the conditions
+#indentation is important 
+# if 2>1:
+#     x='greater';
+# elif 2==1:
+#     x='equal';
+# else:
+#  x='lesser';
+# print(x);
+# # the loop 
+# # syntax
+# x=[1,2,3,4,5,6,7,8];
+# for nums in x:
+#     nums+=1 
+#     # cant change the value of nums
+# print(x);
+# # range function 
+# for num in range (4,10):# inclusive nahi hota h
+#     print(num);
+# i=2;
+# while loop 
+# while i<4:
+#     print(i);
+#     i+=1;
+# the fizzbuzz program
+# for number in range (1,101):
+#     if (number%3==0) and (number %5==0):
+#         print('FizzBuzz');
+#     elif(number%3==0):
+#         print('fizz');
+#     elif(number%5==0):
+#         print('buzz');
+#     else:
+#         print(number);
+
+t=(1,);#we need comma to make the tuple if we wont then it will not be a tuple.
+print(t);
+# print(len(t));
+# print(type(t));
+# continue == pass in python
+#  syntax of function 
+def myfunc(x,y):
+    return x+y;
